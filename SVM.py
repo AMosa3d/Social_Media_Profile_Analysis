593cc278bcbda4a1a929177a318f387a83fe5be4
abdelrahman <abdelrahman.mosa3d.fcis@hotmail.com>
2017-12-16 02:55:34 +0200

    Edited version 1.0

--- SVM.py
+++ SVM.py
@@ -1,12 +1,34 @@
 from sklearn import svm
+import csv
+import time
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-X = [[0,0], [1,1]]
+X = []
+Y = []
 
-Y = [2,3]
+t0 = time.time()
+
+print("1) Reading Data ...")
+with open('Sentiment Analysis Dataset.csv', 'r', encoding="utf8") as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=",")
+    for row in readCSV:
+        X.append(row[3])
+        Y.append(row[1])
+
+print("2) Feature Extracting ...")
+vectorizer = TfidfVectorizer(min_df=0.05, max_df=0.8, sublinear_tf=True, use_idf=True)
+
+print("3) Training ...")
+X = vectorizer.fit_transform(X)
+#Y = vectorizer.fit_transform(Y)
 
 clf = svm.SVC()
+print("4) Fitting ...")
+clf.fit(X[2:100000], Y[2:100000])
 
-clf.fit(X,Y)
 
-hobba = clf.predict([[0.9,0.9]])
+print("5) Predicting ...")
+t = vectorizer.transform(["Have played by Howard Elliot thingy album today.  I really like it.  Well done me"])
+hobba = clf.predict(t)
 print(hobba)
+print(time.time() - t0)
