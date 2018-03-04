9a4de9c893b54ae3159df39079a4b94b5bdca4c1
AMosa3d <abdelrahman.mosa3d.fcis@hotmail.com>
2018-03-04 17:31:44 +0200

    Update getTweets.py
    Hide client secrets

--- webModule/getTweets.py
+++ webModule/getTweets.py
@@ -9,10 +9,10 @@ access_token_secret=""
 maxTweets=200
 def get_tweets(userName):
     #authorize twitter using the keys that we got from dev.twitter
-    consumer_key="IibibQUcX7Ssy1bteWKXFN89G"
-    consumer_secret="XTu1aGLLJqmihDbHL9ofhM51GIfmSuezi64Jieakz6bmVh2hCc"
-    access_token="155015907-KCa96Y9ZXGxSqUTMPHdJ6kmi3pZoZhvjGG1i966A"
-    access_token_secret="mt1k6pWiEU5W7PVNib3YCDXFX7pAChiwCGPaLYhMbhOd4"
+    consumer_key="xxx"
+    consumer_secret="xxx"
+    access_token="xxx-xxx"
+    access_token_secret="xxx"
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
